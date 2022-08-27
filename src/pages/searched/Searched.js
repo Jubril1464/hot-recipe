5d3f61ffb6f2731f547b6cfbbcb0ca359f70782f
Jubril <18-30gd050@students.unilorin.edu.ng>
2022-08-27 08:17:16 +0100

    designed the searched recipes page

--- src/pages/searched/Searched.js
+++ src/pages/searched/Searched.js
@@ -1,47 +1,63 @@
-import { useState, useEffect,useLayoutEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import './Searched.scss'
+import { useState, useEffect, useLayoutEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import "./Searched.scss";
 
 const Searched = () => {
-    const [searchedRecipe, setSearchRecipe] = useState([])
-    const [blur, setBlur] = useState(true);
+  const [searchedRecipe, setSearchRecipe] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const param = useParams();
+  let REACT_APP_RECIPE_API_KEY = "eab71f0e42cb4b79b29a0ac214d577c3";
+
+  const getSearchedRecipes = async (search) => {
+    setLoading(true);
     
-    const param = useParams()
-   let  REACT_APP_RECIPE_API_KEY = "eab71f0e42cb4b79b29a0ac214d577c3";
-
-    const getSearchedRecipes = async (search) => {
-        setBlur(true)
-        const res = await fetch(
-          `https://api.spoonacular.com/recipes/complexSearch?apiKey=eab71f0e42cb4b79b29a0ac214d577c3&query=${search}`
-        );
-        const data = await res.json();
-        return data.results
-
-
+    try {
+      const res = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=eab71f0e42cb4b79b29a0ac214d577c3&query=${search}`
+      );
+      if (!res.ok) {
+        throw new Error('Sorry something went wrong!')
+      }
+      const data = await res.json();
+      return data.results;
+    } catch (err) {
+      setError(err.message)
     }
-    useEffect(() => {
-    console.log('run');
-        let isMounted = true;
-        getSearchedRecipes(param.search).then((data) => {
-            if(isMounted) setSearchRecipe(data)
-        })
-        return () => {
-            isMounted = false
-        }
-    }, [param.search])
-    useLayoutEffect(() => {
-        setBlur(false)
-    })
+  };
+  useEffect(() => {
+    console.log("run");
+    let isMounted = true;
+    getSearchedRecipes(param.search).then((data) => {
+      if (isMounted) setSearchRecipe(data);
+    });
+    setLoading(false)
+
+    return () => {
+      isMounted = false;
+    };
+  }, [param.search]);
+
   return (
-    <div className={`searchedRecipe ${blur ? 'blur' : ''}`}>
-      {searchedRecipe.map(({ title, id, image }) => (
-        <div className="recipeCard" key={id}>
-          <img src={image} alt={title} />
-          <h4>{title}</h4>
-        </div>
-      ))}
+    <div className={`searchedRecipe`}>
+      {loading && <h1>Loading...</h1>}
+      {/* {error && <p>{error}</p>} */}
+
+      <div className="recipeCardContainer">
+        {searchedRecipe ? (
+          searchedRecipe.map(({ title, id, image }) => (
+            <div className="recipeCard" key={id}>
+              <h4 className="recipeCard--title">{title}</h4>
+              <img src={image} alt={title} />
+            </div>
+          ))
+        ) : (
+          <h1>Loading....</h1>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default Searched
+export default Searched;
